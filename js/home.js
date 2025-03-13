d355f2f8611fa690f3fdeb66e235be7e2e197193
mirellaoliveira244 <165833881+mirellaoliveira244@users.noreply.github.com>
2025-03-13 16:42:38 +0000

    alugma coisa

--- js/home.js
+++ js/home.js
@@ -21,3 +21,40 @@ import { verificarAutenticacao } from './autorizar.js';
     conteudo.style.display = 'block';
   } 
 })();
+
+const urlBase = "URL_DO_SEU_BACK_END";
+
+const tabelaCorpo = document.getElementById("tabela-usuarios");
+tabelaCorpo.innerHTML = 'Aguarde...';
+
+try {
+  const endpoint = '/usuario';
+  const urlFinal = urlBase + endpoint;
+  const response = await fetch(urlFinal);
+
+  if (!response.ok) {
+    throw new Error("Erro na requisição: " + response.status);
+  }
+
+  const data = await response.json();
+  tabelaCorpo.innerHTML = '';
+
+  // Loop para preencher a tabela
+  data.forEach(usuario => {
+    const linha = document.createElement("tr");
+    linha.innerHTML = `
+                <td>${usuario.id}</td>
+                <td>${usuario.nome}</td>
+                <td>${usuario.email}</td>
+                <td>${usuario.senha}</td>
+                <td class="acoes">
+                  <a class="botaoVer" href="usuario.html?id=${usuario.id}">Ver</a> | 
+                  <a class="botaoAlterar" href="alterar-usuario.html?id=${usuario.id}">Alterar</a> | 
+                  <a class="botaoExcluir" href="${usuario.id}">Excluir</a>
+                </td>
+            `;
+    tabelaCorpo.appendChild(linha);
+  });
+} catch (error) {
+  console.error("Erro:", error);
+}
